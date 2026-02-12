f8d986b29622c359414958e9f9ae705d3dd3d46d
xfoule <x.foule@gmail.com>
2026-02-12 17:49:44 -0400

    add dossier config

---
+++ backend/config/env.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export const env = {
+  port: process.env.PORT,
+  frontendUrl: process.env.FRONTEND_URL,
+
+  smtp: {
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+
+  mailTo: process.env.MAIL_TO,
+  recaptchaSecret: process.env.RECAPTCHA_SECRET,
+};
