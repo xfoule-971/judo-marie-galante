91b45ad954b3c9e83435a8b6a44cc8a84003d1ca
xfoule <x.foule@gmail.com>
2026-02-12 17:56:34 -0400

    add dossier services

---
+++ backend/services/Mailservice.js
@@ -0,0 +1,12 @@
+import nodemailer from "nodemailer";
+import { env } from "../config/env.js";
+
+export const transporter = nodemailer.createTransport({
+  host: env.smtp.host,
+  port: env.smtp.port,
+  secure: false,
+  auth: {
+    user: env.smtp.user,
+    pass: env.smtp.pass,
+  },
+});
