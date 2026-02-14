3ee04628c49f89cf5edb161e746964a581b8cd2f
xfoule <x.foule@gmail.com>
2026-02-14 11:22:35 -0400

    add mise en place des contenus

---
+++ frontend/js/contact.js
@@ -0,0 +1,20 @@
+document.getElementById("contactForm")
+.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const formData = {
+    name: e.target.name.value,
+    email: e.target.email.value,
+    message: e.target.message.value
+  };
+
+  const response = await fetch("http://localhost:4000/api/contact", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(formData)
+  });
+
+  const data = await response.json();
+  document.getElementById("response").innerText =
+    data.message || "Erreur lors de l'envoi.";
+});
