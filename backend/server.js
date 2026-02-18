2e3ac77eecfbe61df992c3eb424660518e2cefe5
xfoule <x.foule@gmail.com>
2026-02-18 17:27:04 -0400

    add correctif pour render

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,6 @@ import { env } from "./config/env.js";
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(env.port, () => {
+app.listen(env.port, '0.0.0.0',() => {
   console.log(`API sécurisée lancée sur le port ${env.port}`);
 });
